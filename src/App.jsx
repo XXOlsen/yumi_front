726109e8d168fe7c0a603faf493f2112585eb0ed
Chris Hedin Ziegler <60019587+ChriZZZy@users.noreply.github.com>
2023-12-20 13:49:05 +0100

    added useeffect for instant updates

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import facade from './util/apiFacade';
 
 
 
+
 function App() {
   const init = { username: '', password: '' };
   const [loginCredentials, setLoginCredentials] = useState(init);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [dataFromServer, setDataFromServer] = useState('Loading...');
+  const [updateInput, setUpdateInput] = useState({});
 
   useEffect(() => {
     facade.fetchData('hotels', 'GET').then((data) => setDataFromServer(data));
@@ -22,47 +24,43 @@ function App() {
       setIsLoggedIn
     );
   };
+
   const onChange = (evt) => {
     setLoginCredentials({
       ...loginCredentials,
       [evt.target.id]: evt.target.value,
     });
+    // Trigger an update every time input changes
+    setUpdateInput({});
   };
 
   return (
     <>
-
-<header>
-
+      <header>
         <div class="logo">index</div>
         <div class="menu-toggle" id="mobile-menu">
-            <span></span>
-            <span></span>
-            <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
         <nav class="nav">
-            <ul>
-                <li><a href="index.html" class="active">Home</a></li>
-                <li><a href="userpage.html">Page 1</a></li>
-                <li><a href="signup.html">Page 2</a></li>
-            </ul>
+          <ul>
+            <li><a href="index.html" class="active">Home</a></li>
+            <li><a href="userpage.html">Page 1</a></li>
+            <li><a href="signup.html">Page 2</a></li>
+          </ul>
         </nav>
-
-    </header>
+      </header>
 
       <div class="box">
-      
         <h1>Login</h1>
-
         <form onChange={onChange}>
           <input placeholder="Username or user email" id="username" />
           <input placeholder="Password" id="password" />
           <button onClick={performLogin}>Login</button>
           <a href="#" class="btns">Sign up</a>
         </form>
-        </div>
-    
-  
+      </div>
     </>
   );
 }
