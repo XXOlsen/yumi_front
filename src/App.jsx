1b7cfbeccda1744e6fa960c34111052acd5352aa
XXOlsen <cph-xo@cphbusiness.dk>
2023-12-20 14:33:09 +0100

    css til signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { useState, useEffect } from 'react';
-import './App.css';
-import facade from './util/apiFacade';
+import { useState, useEffect } from "react";
+import "./App.css";
+import "./signup.css";
+import facade from "./util/apiFacade";
 
 function App() {
-  const init = { username: '', password: '' };
+  const init = { username: "", password: "" };
   const [loginCredentials, setLoginCredentials] = useState(init);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [dataFromServer, setDataFromServer] = useState('Loading...');
+  const [dataFromServer, setDataFromServer] = useState("Loading...");
 
   useEffect(() => {
-    facade.fetchData('hotels', 'GET').then((data) => setDataFromServer(data));
+    facade.fetchData("diary", "GET").then((data) => setDataFromServer(data));
   }, [isLoggedIn]);
 
   const performLogin = (evt) => {
@@ -27,42 +28,48 @@ function App() {
     });
   };
 
+
   return (
     <>
-
-<header>
-
+        <header>
         <div class="logo">index</div>
         <div class="menu-toggle" id="mobile-menu">
-            <span></span>
-            <span></span>
-            <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
         <nav class="nav">
-            <ul>
-                <li><a href="index.html" class="active">Home</a></li>
-                <li><a href="userpage.html">Page 1</a></li>
-                <li><a href="signup.html">Page 2</a></li>
-            </ul>
+          <ul>
+            <li>
+              <a href="index.html" class="active">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="userpage.jsx">Userpage</a>
+            </li>
+            <li>
+              <a href="signup.jsx">Signup</a>
+            </li>
+          </ul>
         </nav>
-
-    </header>
+      </header>
 
       <div class="box">
-      
         <h1>Login</h1>
 
         <form onChange={onChange}>
           <input placeholder="Username or user email" id="username" />
           <input placeholder="Password" id="password" />
           <button onClick={performLogin}>Login</button>
-          <a href="#" class="btns">Sign up</a>
+          <a href="signup.jsx" class="btns">
+            Sign up
+          </a>
         </form>
-        </div>
-    
-  
+      </div>
     </>
   );
+  
 }
 
 export default App;
