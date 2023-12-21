5b7ccdd63a37a68c5c3e3afbb0ace3069f336574
Chris Hedin Ziegler <60019587+ChriZZZy@users.noreply.github.com>
2023-12-21 10:55:06 +0100

    router stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import './App.css';
 import facade from './util/apiFacade';
-
-
-
+import UserPage from './userPage';
+import AdminPage from './adminpage'; // assuming you have an AdminPage component
 
 function App() {
   const init = { username: '', password: '' };
   const [loginCredentials, setLoginCredentials] = useState(init);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [dataFromServer, setDataFromServer] = useState('Loading...');
-  const [updateInput, setUpdateInput] = useState({});
+  const [isAdmin, setIsAdmin] = useState(false);
 
   useEffect(() => {
     facade.fetchData('hotels', 'GET').then((data) => setDataFromServer(data));
@@ -30,38 +30,44 @@ function App() {
       ...loginCredentials,
       [evt.target.id]: evt.target.value,
     });
-    // Trigger an update every time input changes
-    setUpdateInput({});
   };
 
   return (
-    <>
+    <Router>
       <header>
-        <div class="logo">index</div>
-        <div class="menu-toggle" id="mobile-menu">
+        <div className="logo">index</div>
+        <div className="menu-toggle" id="mobile-menu">
           <span></span>
           <span></span>
           <span></span>
         </div>
-        <nav class="nav">
+        <nav className="nav">
           <ul>
-            <li><a href="index.html" class="active">Home</a></li>
-            <li><a href="userpage.html">Page 1</a></li>
+            <li><NavLink to="/" activeclassname="active">Home</NavLink></li>
+            <li><NavLink to="/user" activeclassname="active">User Page</NavLink></li>
+            <li><NavLink to="/admin" activeclassname="active">Admin Page</NavLink></li>
             <li><a href="signup.html">Page 2</a></li>
           </ul>
         </nav>
       </header>
 
-      <div class="box">
+      <div className="box">
         <h1>Login</h1>
         <form onChange={onChange}>
           <input placeholder="Username or user email" id="username" />
           <input placeholder="Password" id="password" />
           <button onClick={performLogin}>Login</button>
-          <a href="#" class="btns">Sign up</a>
+          <a href="#" className="btns">Sign up</a>
         </form>
       </div>
-    </>
+
+
+      <routes>
+      <Route path="/user" component={UserPage} />
+      <Route path="/admin" render={() => <AdminPage isAdmin={isAdmin} />} />
+      {/* Add other Routes as necessary */}
+      </routes>
+    </Router>
   );
 }
 
--- src/userpage.jsx
+++ src/userpage.jsx
@@ -1,10 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import './userpage.css';
 import facade from './util/apiFacade';
 
-
-function App() {
+function UserPage() {
     const init = { username: '', password: '' };
     const [loginCredentials, setLoginCredentials] = useState(init);
     const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -22,6 +21,7 @@ function App() {
             setIsLoggedIn
         );
     };
+
     const onChange = (evt) => {
         setLoginCredentials({
             ...loginCredentials,
@@ -29,66 +29,11 @@ function App() {
         });
     };
 
-
     return (
-        
-            <div class="userflex">
-                <div class="flex-item1">
-                    <h3>diary (hashmapp)</h3>
-                </div>
-
-
-                <div class="user-flex-item flex-item2">
-
-
-
-
-                    <div class="diary">
-
-
-                        <div class="diary-items diary-item1">
-                            <form action="userpage.php" method="post">
-                                <input type="text" name="date" placeholder="Date"/>
-                            </form>
-                        </div>
-
-
-                        <div class="diary-items diary-item2">
-                            <form action="userpage.php" method="post">
-                                <input type="text" name="topic" placeholder="todays topic"/>
-                            </form>
-                        </div>
-
-
-                        <div class="diary-items diary-item3">
-                            <form action="userpage.php" method="post">
-                                <input type="text" name="mood" placeholder="pick your mood"/>
-                            </form>
-                        </div>
-
-
-
-                        <div class="diary-items diary-item4">
-                            <form>
-                                <label for="multilineInput">write your diary here</label>
-                                <textarea class="diary-box" id="multilineInput" name="multilineInput" rows="8"
-                                    cols="25"></textarea>
-                                <input type="submit" value="save page in diary"/>
-                            </form>
-                        </div>
-
-
-
-
-
-                    </div>
-
-
-
-                </div>
-            </div>
-
-            );
-
+        <div className="userflex">
+            {/* Your UI components and JSX here */}
+        </div>
+    );
 }
-export default userpage
+
+export default UserPage;
