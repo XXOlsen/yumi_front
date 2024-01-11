41052b766f88d529e84eadc0e0cadba2588a96bf
Andreas Pihl <54024731+bakkary@users.noreply.github.com>
2024-01-11 23:53:18 +0100

    made some flex boxes to admin page

--- src/adminpage.jsx
+++ src/adminpage.jsx
@@ -1,11 +1,25 @@
-
+import './css/admin.css';
 
 function AdminPage() {
     return (
-        <div>
+     
+       <div>
         <h1>Admin Page</h1>
         <p>Only for admins</p>
+<div class ="flex">
+        <div class="flexexamplep">
+         <label htmlFor="username">Username:</label>
+    <input type="text"  name="username" placeholder="Enter your username" />
+
+    <label htmlFor="password">Password:</label>
+    <input type="password"  name="password" placeholder="Enter your password" />
+
+    <button type="submit">Login</button>
+    </div>
+        </div> 
         </div>
+ 
+        
     );
     }
 
