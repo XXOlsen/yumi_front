efc318053d324ac52a494041fe2d448697bbe962
Chris Hedin Ziegler <60019587+ChriZZZy@users.noreply.github.com>
2024-01-16 10:51:39 +0100

    .

--- src/Examples/adminpageOLD/adminpage.jsx
+++ src/Examples/adminpageOLD/adminpage.jsx
@@ -1,54 +1,26 @@
-import { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, NavLink, Routes} from 'react-router-dom';
-import './css/App.css';
-import facade from './util/apiFacade';
-import Login from './loginpage';
-import Signup from './signup';
-import UserPage from './userPage';
-import AdminPage from './adminpage'; 
+import './css/admin.css';
 
+function AdminPage() {
+    return (
+     
+       <div>
+        <h1>Admin Page</h1>
+        <p>Only for admins</p>
+<div class ="flex">
+        <div class="flexexamplep">
+         <label htmlFor="username">Username:</label>
+    <input type="text"  name="username" placeholder="Enter your username" />
 
-function App() {
-  const [isAdmin, setIsAdmin] = useState(false);
+    <label htmlFor="password">Password:</label>
+    <input type="password"  name="password" placeholder="Enter your password" />
 
-
-
-  return (
-    <Router>
-      <header>
-      <img className="logo" src="/assets/logo2.png" alt="yumiLogo" onError={(e) => console.log("Error loading image:", e)}/>
-        <div className="menu-toggle" id="mobile-menu">
-          <span></span>
-          <span></span>
-          <span></span>
+    <button type="submit">Login</button>
+    </div>
+        </div> 
         </div>
-        <nav className="nav">
-          <ul>
-            <li><NavLink to="/" activeClassName="active">Home</NavLink></li>
-            <li><NavLink to="/loginpage" activeClassName="active">Login</NavLink></li>
-            <li><NavLink to="/user" activeClassName="active">User Page</NavLink></li>
-            <li><NavLink to="/admin" activeClassName="active">Admin Page</NavLink></li>
-            <li><NavLink to="/signup" activeClassName="active">Signup</NavLink></li>
-          </ul>
-        </nav>
-      </header>
-    
+ 
+        
+    );
+    }
 
-   
-
-
-      <Routes>
-        <Route path="/loginpage" element={<Login/>} />
-      <Route path="/signup" element={<Signup/>} />
-    <Route path="/user" element={<UserPage/>} />
-    <Route path="/admin" element={<AdminPage isAdmin={isAdmin} />} />
-      {/* Add other Routes as necessary */}
-      </Routes>
-
-      </Router>  
-    
-  );
-  
-}
-
-export default App;
+export default AdminPage;
